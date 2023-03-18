625858b6730c8690b8f03ca7a492116b65a9f8bb
alejandro waumann <awaumann@gmail.com>
2023-03-18 17:47:57 -0500

    Add permanent drawer for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
-import { AppBar, Avatar, Button, CssBaseline, ThemeProvider, Toolbar, Typography } from '@mui/material'
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  CssBaseline,
+  Drawer,
+  Stack,
+  ThemeProvider,
+  Toolbar,
+  Typography
+} from '@mui/material'
 
 import darkTheme from './darkTheme';
 
@@ -8,13 +19,23 @@ function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline enableColorScheme />
-      <AppBar position="relative" color="transparent" elevation={0}>
+
+      <AppBar position="relative" color="transparent" elevation={0} variant="outlined">
         <Toolbar>
-          <Avatar src="/logo.jpg" sx={{ mr: 2 }} />
-          <Typography variant="h6" flexGrow={1}>Complexjty</Typography>
+          <Box mr={9} />
+          <Typography variant="h6" flexGrow={1}>Page Title</Typography>
           <Button variant="outlined">Connect Wallet</Button>
         </Toolbar>
       </AppBar>
+
+      <Drawer variant="permanent">
+        <Box alignItems="center" sx={{ width: 72, pt: 2 }}>
+          <Stack direction="column" alignItems="center" justifyContent="flex-start" spacing={2}>
+            <Avatar src="/logo.jpg" />
+          </Stack>
+        </Box>
+      </Drawer>
+
     </ThemeProvider>
   )
 }
