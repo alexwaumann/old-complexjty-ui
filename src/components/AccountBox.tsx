01335c60b357928f3d5c79a7d6479a65671097fb
alejandro waumann <awaumann@gmail.com>
2023-03-23 23:09:39 -0500

    Add account menu to AccountBox

--- src/components/AccountBox.tsx
+++ src/components/AccountBox.tsx
@@ -1,9 +1,15 @@
 import {
   Avatar,
+  Badge,
   Box,
+  Divider,
   IconButton,
+  Menu,
+  MenuItem,
+  Stack,
   Typography,
 } from "@mui/material";
+import {useState} from "react";
 
 import {
   useAccount,
@@ -12,19 +18,30 @@ import {
 /*
  * TODO
  * Avatar border color based on player rank (determined by server rr)
- * Avatar should be a menu button
  * Menu should display:
  *    * Address and rank info
  *    * Wallet network status
  *    * Verification status (enables social features)
  *    * Settings
  *    * Disconnect option (should disconnect wallet + remove reset verification status)
+ * Display a way for the user to connect wallet to correct network
  */
 
 const AccountBox = () => {
   const walletAddress = useAccount();
   // const displayName = 'Godyl'; // useAuth((state) => state.displayName);
   const displayName = `${walletAddress?.slice(0, 5)}...${walletAddress?.slice(38)}`;
+
+  const [menuAnchor, setMenuAnchor] = useState<null | HTMLElement>(null);
+  const menuOpen = Boolean(menuAnchor);
+
+  const handleAvatarClicked = (event: React.MouseEvent<HTMLElement>) => {
+    setMenuAnchor(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setMenuAnchor(null);
+  };
 
   return (
     <Box
@@ -40,12 +57,49 @@ const AccountBox = () => {
 
       <Box m={1} />
 
-      <IconButton sx={{ padding: 0 }}>
+      <IconButton onClick={handleAvatarClicked} sx={{ padding: 0 }}>
         <Avatar
           src="/placeholder-pfp.webp"
           sx={{ border: 1, borderWidth: 2, borderColor: 'white' }}
         />
       </IconButton>
+
+      <Menu
+        anchorEl={menuAnchor}
+        open={menuOpen}
+        onClose={handleCloseMenu}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        PaperProps={{ elevation: 0, sx: { mt: 1, borderRadius: 2, color: 'white', bgcolor: '#ffffff44' } }}
+      >
+        <MenuItem>
+          <Stack direction="row" alignItems="center">
+            <Badge variant="dot" color="success" />
+            <Box m={1} />
+            <Typography variant="caption">{walletAddress}</Typography>
+          </Stack>
+        </MenuItem>
+
+        <Divider />
+        <MenuItem>
+          <Typography variant="caption">Get verified to access social features</Typography>
+        </MenuItem>
+
+        <Divider />
+        <MenuItem>
+          <Typography variant="caption">Profile</Typography>
+        </MenuItem>
+
+        <Divider />
+        <MenuItem>
+          <Typography variant="caption">Settings</Typography>
+        </MenuItem>
+
+        <Divider />
+        <MenuItem>
+          <Typography variant="caption">Disconnect</Typography>
+        </MenuItem>
+      </Menu>
     </Box>
   )
 };
