916b1fcf26e54b6a0294790025c4050bc3105377
alejandro waumann <awaumann@gmail.com>
2023-03-26 12:55:07 -0500

    Align menu with AccountBox

--- src/components/AccountBox.tsx
+++ src/components/AccountBox.tsx
@@ -2,8 +2,8 @@ import {
   Avatar,
   Badge,
   Box,
+  ButtonBase,
   Divider,
-  IconButton,
   Menu,
   MenuItem,
   Stack,
@@ -14,6 +14,9 @@ import {useState} from "react";
 import {
   useAccount,
 } from "../connectors/metamask";
+
+// TODO: TOMORROW
+// show state of connection with identifiers
 
 /*
  * TODO
@@ -29,8 +32,8 @@ import {
 
 const AccountBox = () => {
   const walletAddress = useAccount();
-  // const displayName = 'Godyl'; // useAuth((state) => state.displayName);
-  const displayName = `${walletAddress?.slice(0, 5)}...${walletAddress?.slice(38)}`;
+  const displayName = 'Godyl'; // useAuth((state) => state.displayName);
+  const displayAddress = `${walletAddress?.slice(0, 5)}...${walletAddress?.slice(38)}`;
 
   const [menuAnchor, setMenuAnchor] = useState<null | HTMLElement>(null);
   const menuOpen = Boolean(menuAnchor);
@@ -44,30 +47,22 @@ const AccountBox = () => {
   };
 
   return (
-    <Box
-      height={52}
-      color="000000"
-      bgcolor="#ffffff33"
-      borderRadius={2}
-      paddingLeft={2}
-      paddingRight={1}
-      display="flex"
-      alignItems="center"
-    >
-      <Stack direction="row" alignItems="center">
-        <Badge variant="dot" color="success" />
-        <Box m={1} />
-        <Typography variant="caption">{displayName}</Typography>
-      </Stack>
-
-      <Box m={1} />
-
-      <IconButton onClick={handleAvatarClicked} sx={{ padding: 0 }}>
-        <Avatar
-          src="/placeholder-pfp.webp"
-          sx={{ border: 1, borderWidth: 2, borderColor: 'white' }}
-        />
-      </IconButton>
+    <>
+      <ButtonBase onClick={handleAvatarClicked} sx={{ height: 52, bgcolor: '#444', borderRadius: 2 }}>
+        <Stack direction="row" alignItems="center" spacing={2} mx={1}>
+          <Stack direction="column">
+            <Typography variant="caption">{displayName}</Typography>
+            <Typography variant="caption">{displayAddress}</Typography>
+          </Stack>
+          <Avatar
+            src="/placeholder-pfp.webp"
+            sx={{
+              border: 1,
+              borderWidth: 2,
+            }}
+          />
+        </Stack>
+      </ButtonBase>
 
       <Menu
         anchorEl={menuAnchor}
@@ -75,7 +70,7 @@ const AccountBox = () => {
         onClose={handleCloseMenu}
         transformOrigin={{ horizontal: 'right', vertical: 'top' }}
         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-        PaperProps={{ elevation: 0, sx: { mt: 1, borderRadius: 2, color: 'white', bgcolor: '#ffffff33' } }}
+        PaperProps={{ sx: { mt: 1, borderRadius: 2, background: '#444' } }}
       >
         <MenuItem sx={{ pointerEvents: 'none' }}>
           <Stack direction="row" alignItems="center">
@@ -105,7 +100,7 @@ const AccountBox = () => {
           <Typography variant="caption">Disconnect</Typography>
         </MenuItem>
       </Menu>
-    </Box>
+    </>
   )
 };
 
