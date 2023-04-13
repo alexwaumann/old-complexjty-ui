d3e5fbc8db55f2d99fc5ab14c24a49664f79a755
alejandro waumann <awaumann@gmail.com>
2023-04-13 01:34:27 -0500

    Remove @web3-react/network package

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import { connectNetwork } from './connectors/network';
 import { connectWalletEagerly } from './hooks/useAuth';
 
-connectNetwork();
 connectWalletEagerly();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
