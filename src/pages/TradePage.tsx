dc9dea170e003236c99ee3b75ce87f5e7eb42558
alejandro waumann <awaumann@gmail.com>
2023-04-17 23:04:53 -0500

    Move trade UI into a Trade component

--- src/pages/TradePage.tsx
+++ src/pages/TradePage.tsx
@@ -1,142 +1,16 @@
-import { TrendingDownRounded, TrendingUpRounded} from "@mui/icons-material";
-import { Box, Button, Container, Grid, InputAdornment, MenuItem, Paper, Select, Slider, Stack, TextField, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
-import { useState } from "react";
+import { Box, Container, Grid, Paper } from "@mui/material";
 
-import useAuth from "../hooks/useAuth";
-import { oraclePricesSelector, useOracle } from "../services/oracle";
-import { SupportedToken, TOKEN, TOKENS } from "../services/tokens";
-import { formatTokenAmount, formatUsdPrice } from "../utils/format";
-import { PAIRS, PAIR, SupportedPair } from "../utils/pairs";
+import Trade from "../components/Trade";
+
 
 const TradePage = () => {
-  const positions = [1, 2, 3, 4, 5];
-  const oracle = useOracle(oraclePricesSelector);
-  const balances = useAuth((state) => state.balances);
-
-  const [pairName, setPairName] = useState<SupportedPair>('WETH/USDC');
-  const [tradeType, setTradeType] = useState<'LONG' | 'SHORT'>('LONG');
-
-  const [fundingToken, setFundingToken] = useState<SupportedToken>('USDC');
-  const [fundingAmount, setFundingAmount] = useState<string>('');
-  const fundingError: boolean = Number(fundingAmount) > balances[fundingToken];
-
-  const pair = PAIR[pairName];
-  const price = oracle[pair[0].name] / oracle[pair[1].name];
-
-  const debtToken       = tradeType === 'LONG' ? pair[1] : pair[0];
-  const collateralToken = tradeType === 'LONG' ? pair[0] : pair[1];
+  const positions = [1, 2, 3];
 
   return (
     <Container maxWidth="xl">
+      <Trade />
+      <Box m={2} />
       <Grid container spacing={2}>
-
-        <Grid item xs={12}>
-          <Paper sx={{ p: 2, borderRadius: 2 }}>
-            <Stack direction="row" alignItems="center" spacing={4}>
-              <Select
-                value={pairName}
-                onChange={(event) => setPairName(event.target.value as SupportedPair)}
-                color="primary"
-              >
-                {PAIRS.map((pairName) => <MenuItem key={pairName} value={pairName}>{pairName}</MenuItem>)}
-              </Select>
-              <Typography fontWeight={700}>{formatTokenAmount(pair[1].name, price)}</Typography>
-            </Stack>
-          </Paper>
-        </Grid>
-
-        <Grid item xs={12} md={6}>
-          <Paper sx={{ height: 300, p: 2, borderRadius: 2 }}>
-            <Stack justifyContent="space-between" height="100%">
-              <ToggleButtonGroup
-                value={tradeType}
-                onChange={(event, value: "LONG" | "SHORT" | null) => { if(value !== null) setTradeType(value) }}
-                exclusive
-                color="primary"
-                fullWidth
-              >
-                <ToggleButton value="LONG">
-                  <TrendingUpRounded />
-                  <Box m={1} />
-                  <Typography>LONG</Typography>
-                </ToggleButton>
-                <ToggleButton value="SHORT">
-                  <TrendingDownRounded />
-                  <Box m={1} />
-                  <Typography>SHORT</Typography>
-                </ToggleButton>
-              </ToggleButtonGroup>
-
-              <TextField
-                label={`Pay: $${formatUsdPrice(Number(fundingAmount) * oracle[fundingToken])}`}
-                value={fundingAmount ? fundingAmount : ''}
-                InputLabelProps={{ shrink: true }}
-                placeholder={'0.00'}
-                helperText={`Balance: ${balances[fundingToken]}`}
-                error={fundingError}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <Button onClick={() => setFundingAmount(balances[fundingToken].toString())}>MAX</Button>
-                      <Select
-                        variant="standard"
-                        value={fundingToken}
-                        onChange={(event) => {
-                          setFundingToken(event.target.value as SupportedToken)
-                          setFundingAmount('');
-                        }}
-                        color="primary"
-                        disableUnderline
-                      >
-                        {TOKENS.map((tokenName) => <MenuItem key={tokenName} value={tokenName}>{tokenName}</MenuItem>)}
-                      </Select>
-                    </InputAdornment>
-                  )
-                }}
-                onChange={(event) => {
-                  const amount = event.target.value;
-                  if(isNaN(Number(amount))) return;
-
-                  setFundingAmount(amount);
-                }}
-                fullWidth
-              />
-
-              <Stack direction="row" alignItems="center" justifyContent="space-between">
-                <Typography>Leverage</Typography>
-                <Typography></Typography>
-              </Stack>
-
-              <Slider
-                aria-label="Leverage Multiplier"
-                defaultValue={2}
-                step={0.1}
-                min={1.5}
-                max={TOKEN[fundingToken].maxLeverage}
-                valueLabelDisplay="auto"
-                size="small"
-              />
-              <Button variant="outlined" disabled={!fundingAmount || fundingError}>Open position</Button>
-            </Stack>
-          </Paper>
-        </Grid>
-
-        <Grid item xs={12} md={6}>
-          <Paper sx={{ height: 300, p: 2, borderRadius: 2 }}>
-            <Stack spacing={2}>
-
-              <Stack direction="row" alignItems="center" justifyContent="space-between">
-                <Typography variant="subtitle1" fontWeight={700}>Funding</Typography>
-                <Stack alignItems="flex-end">
-                  <Typography variant="caption" fontWeight={700}>{formatTokenAmount(fundingToken, Number(fundingAmount))} {fundingToken}</Typography>
-                  <Typography variant="caption">${formatUsdPrice(Number(fundingAmount) * oracle[fundingToken])}</Typography>
-                </Stack>
-              </Stack>
-
-            </Stack>
-          </Paper>
-        </Grid>
-
         {positions.map((value) => (
           <Grid item key={value} xs={12} md={6} lg={4}>
             <Paper sx={{ height: 250, p: 2, borderRadius: 2 }}>
@@ -144,7 +18,6 @@ const TradePage = () => {
             </Paper>
           </Grid>
         ))}
-
       </Grid>
     </Container>
   )
